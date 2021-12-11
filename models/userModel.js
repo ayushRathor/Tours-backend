18f5279468a9ef98d6bf4af8c8b98292a3f2a2fe
ayush <ayushh.rathor@gmail.com>
2021-12-11 23:35:08 +0530

    userModel added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name,email,photo,password,passwordConfirm
 
@@ -9,7 +10,7 @@ const userSchema = new mongoose.Schema({
     required: [true, 'name is required'],
     trim: true,
     maxlength: [40, 'name must have less or equal then 40 characters'],
-    minlength: [10, 'name must have more or equal then 10 characters'],
+    minlength: [2, 'name must have more or equal then 10 characters'],
   },
   email: {
     type: String,
@@ -32,7 +33,23 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Confirm password is required'],
+    //This will only work with create and save
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
